ce3450514bbcd0140c942a22b3de99036a28edf5
Dinika <dinikasaxenas@gmail.com>
2018-11-26 00:31:52 +0530

    update modal & ordersummary when props change to improve performance
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../HOC/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop showBackdrop={props.showModal} onBackdropClicked={props.onPurchaseCancelled} />
-            <div className={classes.Modal}
-                style={{
-                    transform: props.showModal ? 'translateY(0)' : 'translate(-100vh)',
-                    opacity: props.showModal ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-        </Aux>
-    )
+class Modal extends Component {
+    UNSAFE_componentWillUpdate() {
+        console.log('Modal WillUpdate');
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.showModal !== nextProps.showModal
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop showBackdrop={this.props.showModal} onBackdropClicked={this.props.onPurchaseCancelled} />
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.showModal ? 'translateY(0)' : 'translate(-100vh)',
+                        opacity: this.props.showModal ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
 }
 
 export default Modal;
