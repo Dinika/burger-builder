ce7a866dc3df08e1c852df570292ebfafdd84de1
Dinika <dinikasaxenas@gmail.com>
2018-12-02 06:29:35 +0530

    fetch orders from firebase
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,7 +1,31 @@
 import React, {Component} from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-order';
 
 class Orders extends Component {
+    state = {
+        orders: null,
+        loading: true,
+    }
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                let fetchedOrders = [];
+                for(let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        key: key,
+                    })
+                }
+                this.setState({
+                    orders: fetchedOrders,
+                    loading: false,
+                })
+            })
+            .catch(error => {
+                this.setState({loading: false});
+            })
+    }
     render(){
         return(
             <div>
