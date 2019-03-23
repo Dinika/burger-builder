adf958391f2c429847969bd9b455e63a439cf402
Dinika <dinikasaxenas@gmail.com>
2019-03-23 17:59:29 +0530

    outsource checkvalidity
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import checkValidity from '../../utils/checkValidity';
 import InputElement from '../../components/UI/InputElement/InputElement';
 import Button from '../../components/UI/Button/Button';
 import classes from '../Auth/Auth.module.css';
@@ -48,31 +49,6 @@ class Auth extends Component {
     }
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    return isValid;
-  }
-
   onInputChanged(event, inputIdentifier) {
     const updatedControls = {
       ...this.state.controls,
@@ -80,7 +56,7 @@ class Auth extends Component {
         ...this.state.controls[inputIdentifier],
         touched: true,
         value: event.target.value,
-        isValid: this.checkValidity(
+        isValid: checkValidity(
           event.target.value,
           this.state.controls[inputIdentifier].rules
         )
--- src/containers/Checkout/ContactForm/ContactForm.jsx
+++ src/containers/Checkout/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import checkValidity from '../../../utils/checkValidity';
 import classes from './ContactForm.module.css';
 import Button from '../../../components/UI/Button/Button';
 import Loader from '../../../components/UI/Loader/Loader';
@@ -102,37 +103,12 @@ class ContactForm extends Component {
     this.props.onPurchaseBurgerStart(this.props.tokenId, order);
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-    return isValid;
-  }
-
   onInputChanged(event, inputIdentifier) {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedInputElement = { ...this.state.orderForm[inputIdentifier] };
     let isFormValid = true;
     updatedInputElement.value = event.target.value;
-    const isValid = this.checkValidity(
+    const isValid = checkValidity(
       event.target.value,
       updatedInputElement.rules
     );
